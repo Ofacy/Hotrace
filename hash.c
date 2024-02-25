50e5d2a666fceb97165b3ab6e4776f2167996f4b
Ofacy <lcottet@student.42lyon.fr>
2024-02-25 17:55:56 +0100

    Change unmodified variable to be hard coded

--- hash.c
+++ hash.c
@@ -6,7 +6,7 @@
 /*   By: lcottet <lcottet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:11:06 by lcottet           #+#    #+#             */
-/*   Updated: 2024/02/25 14:20:07 by lcottet          ###   ########.fr       */
+/*   Updated: 2024/02/25 17:50:39 by lcottet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 t_hash	hash_str(char *str, size_t *len)
 {
 	t_hash	a;
-	t_hash	b;
 	t_hash	hash;
 	size_t	i;
 
 	hash = 0;
 	a = 63689;
-	b = 378551;
 	i = 0;
 	while (str[i])
 	{
 		hash = hash * a + str[i];
-		a *= b;
+		a *= 378551;
 		i++;
 	}
 	if (len)
